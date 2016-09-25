ae9f168b78ce3eff6c9933d7cd31884a4f24d9fb
wilson <309150945@qq.com>
2016-09-25 10:23:15 +0800

    view commit

--- app/src/main/java/com/lj/apps/calendardemo/MainActivity.java
+++ app/src/main/java/com/lj/apps/calendardemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.lj.apps.calendardemo;
 
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class MainActivity extends AppCompatActivity {
+    MaterialCalendarView mcv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
+        show();
+    }
+
+
+    private void show() {
+        mcv.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(2015, 4, 3))
+                .setMaximumDate(CalendarDay.from(2017, 5, 12))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
     }
 }
