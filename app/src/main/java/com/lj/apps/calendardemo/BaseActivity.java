270e592b55d17c710d541df207f3176b756272c6
lijun <309150945@qq.com>
2016-10-21 09:58:03 +0800

    first commit

---
+++ app/src/main/java/com/lj/apps/calendardemo/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.lj.apps.calendardemo;
+
+import com.smartydroid.android.starter.kit.app.StarterActivity;
+
+/**
+ * Created by Administrator on 2016/10/21.
+ */
+public class BaseActivity extends StarterActivity {
+}
--- app/src/main/java/com/lj/apps/calendardemo/MainActivity.java
+++ app/src/main/java/com/lj/apps/calendardemo/MainActivity.java
@@ -1,40 +1,19 @@
 package com.lj.apps.calendardemo;
 
-import android.icu.util.Calendar;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-import com.prolificinteractive.materialcalendarview.CalendarDay;
-import com.prolificinteractive.materialcalendarview.CalendarMode;
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
-import com.youth.banner.Banner;
 
-public class MainActivity extends AppCompatActivity {
-    MaterialCalendarView mcv;
-    Banner banner;
-    String[] images = new String[]{"url"};
+public class MainActivity extends BaseActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mcv = (MaterialCalendarView) findViewById(R.id.calendarView);
-        banner = (Banner) findViewById(R.id.banner);
-        show();
+
 
     }
 
 
-    private void show() {
-        mcv.state().edit()
-                .setFirstDayOfWeek(Calendar.SUNDAY)
-                .setMinimumDate(CalendarDay.from(2015, 4, 3))
-                .setMaximumDate(CalendarDay.from(2017, 5, 12))
-                .setCalendarDisplayMode(CalendarMode.MONTHS)
-                .commit();
-    }
-
-    private void showBanner() {
-        banner.setImages(images);
-    }
 }
